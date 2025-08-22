8e25ee3963e01d560d542e6278d853de2ed6baa3
flowey-stack <lillian0525flowey@gmail.com>
2025-08-22 22:39:39 +0800

    轉場
    做一半存一下

---
+++ component/scene_trigger.gd
@@ -0,0 +1,9 @@
+class_name SceneTrigger extends Area2D
+
+@export var connected_scene : String #要轉換的場景
+var scene_folder = "res://scenes/objects/room/"
+
+func _on_body_entered(body: Node2D) -> void:
+	var full_path = scene_folder + connected_scene + ".tscn"
+	var scene_tree = get_tree()
+	scene_tree.call_deferred("change_scene_to_file", full_path)
