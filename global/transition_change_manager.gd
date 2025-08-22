3fb21e3f9f3d35e5047b9c0242660bbee4bcfa1d
flowey-stack <lillian0525flowey@gmail.com>
2025-08-23 00:48:19 +0800

    轉場
    有轉出去就很棒了(睡覺

--- global/transition_change_manager.gd
+++ global/transition_change_manager.gd
@@ -19,4 +19,28 @@ func fade_out() :
 	color_rect.visible = true
 	
 	var tween = get_tree().create_tween()
-	tween.tween_property(color_rect, "modulate.a", 1, transition_time)
+	tween.tween_property(color_rect, "modulate:a", 1, transition_time)
+	tween.finished.connect(on_fade_out_completed)
+
+func on_fade_out_completed():
+	get_tree().change_scene_to_file(next_scene_path)
+	fade_in()
+
+func fade_in():
+	color_rect.modulate.a = 1
+	var tween = get_tree().create_tween()
+	tween.tween_property(color_rect, "modulate:a", 0, transition_time)
+	
+	tween.finished.connect(on_fade_in_finished)
+
+func on_fade_in_finished():
+	is_transitioning = false
+	transition_done.emit()
+
+func change_scene(next_scene_path: String):
+	if is_transitioning:
+		return
+	
+	self.next_scene_path = next_scene_path
+	fade_out()
+	
