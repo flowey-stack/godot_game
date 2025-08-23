1ac48c5469f8d4aa480f92cd14c7e77bd6b0647f
flowey-stack <lillian0525flowey@gmail.com>
2025-08-23 16:55:41 +0800

    轉場

--- scenes/room/room_2.gd
+++ scenes/room/room_2.gd
@@ -1,14 +1,13 @@
 extends Node2D
 const PLAYER_SCENE = preload("res://scenes/characters/players/player.tscn")
-@onready var player_spawn_place_marker: Marker2D = $PlayerSpawnPlace
+@onready var player_spawn_place_marker: Marker2D = $PlayerSpawnPlace #改這裡
 
 func _ready() -> void:
 	TransitionChangeManager.transition_done.connect(on_transition_done)
 	var player = PLAYER_SCENE.instantiate()
+	self.add_child(player)
 	player.set_physics_process(false)
 	player.set_process_input(false)
-	self.add_child(player)
-	
 	player.position = player_spawn_place_marker.position
 
 func on_transition_done():
@@ -17,4 +16,4 @@ func on_transition_done():
 
 
 func _on_exit_area_body_entered(body: Node2D) -> void:
-	TransitionChangeManager.change_scene("res://scenes/tests/test_change_scene.tscn")
+	TransitionChangeManager.change_scene("res://scenes/tests/test_change_scene.tscn") #改這裡
