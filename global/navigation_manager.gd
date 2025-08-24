ad9d18d09e833f6f6d0f9f78f2948c4e3b3faa97
s111116037-art <s111116037@stu.ntue.edu.tw>
2025-08-24 20:46:15 +0800

    轉場
    呵呵終於用出來 差點撞牆

--- global/navigation_manager.gd
+++ global/navigation_manager.gd
@@ -1,4 +1,24 @@
 extends Node
 
-const scene_main = preload("res://scenes/level/world/world.tscn")
-const level_mine = preload("res://scenes/level/mine/level_mine.tscn")
+const scene_world = preload("res://scenes/level/world/world.tscn")
+const scene_mine = preload("res://scenes/level/mine/mine.tscn")
+
+signal on_trigger_player_spawn 
+
+var spawn_door_tag
+
+func go_to_level(level_tag, destination_tag):
+	var scene_to_load
+	
+	match level_tag:
+		"world" :
+			scene_to_load = scene_world
+		"mine" :
+			scene_to_load = scene_mine
+		
+	if scene_to_load != null:
+		spawn_door_tag = destination_tag
+		get_tree().change_scene_to_packed(scene_to_load)
+
+func trigger_player_spawn(position: Vector2, direction: String):
+	on_trigger_player_spawn.emit(position, direction)
--- scenes/component/scene_door/door.gd
+++ scenes/component/scene_door/door.gd
@@ -1,8 +1,8 @@
 class_name Door
 extends Area2D
 
-@export var destination_level : String #要抵達的場景
-@export var destination_door : String #要抵達的門名稱
+@export var destination_level_tag : String #要抵達的場景
+@export var destination_door_tag : String #要抵達的門名稱
 @export var spawn_direction = "up" #碰到spawn時的spawn方向
 
 @onready var spawn = $Spawn
@@ -10,4 +10,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		pass
+		NavigationManager.go_to_level(destination_level_tag, destination_door_tag)
