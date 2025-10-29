439210cf84e0b70dcc7ab4055663c0c6be84394e
s111116037-art <s111116037@stu.ntue.edu.tw>
2025-10-29 18:29:06 +0800

    inventory

--- scenes/objects/inventory/inventory.gd
+++ scenes/objects/inventory/inventory.gd
@@ -3,12 +3,16 @@ class_name Inventory
 
 signal updated
 
-@export var items : Array[InventoryItem]
+@export var slots : Array[InventorySlot]
 
 func insert(item : InventoryItem):
-	for i in range(items.size()):
-		if !items[i]:
-			items[i] = item
-			break
+	var item_slots = slots.filter(func(slot): return slot.item == item)
+	if !item_slots.is_empty():
+		item_slots[0].amount +=1
+	else:
+		var empty_slots = slots.filter(func(slot): return slot.item == null)
+		if !empty_slots.is_empty():
+			empty_slots[0].item = item
+			empty_slots[0].amount = 1
 	
 	updated.emit()
--- scenes/ui/inventory/slot.gd
+++ scenes/ui/inventory/slot.gd
@@ -3,11 +3,11 @@ extends Panel
 @onready var back_ground_sprite : Sprite2D = $BackGround
 @onready var item_sprite : Sprite2D = $CenterContainer/Panel/Item
 
-func update(item : InventoryItem):
-	if !item:
+func update(slot : InventorySlot):
+	if !slot.item:
 		back_ground_sprite.frame = 0
 		item_sprite.visible = false
 	else:
 		back_ground_sprite.frame = 1
 		item_sprite.visible = true
-		item_sprite.texture = item.texture
+		item_sprite.texture = slot.item.texture
