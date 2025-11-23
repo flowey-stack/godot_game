7a16d5703c7eec3bd1d3e040361c8ef63d9547b2
s111116037-art <s111116037@stu.ntue.edu.tw>
2025-11-24 03:09:26 +0800

    物品欄

--- scenes/objects/inventory/inventory.gd
+++ scenes/objects/inventory/inventory.gd
@@ -16,3 +16,12 @@ func insert(item : InventoryItem):
 			empty_slots[0].amount = 1
 	
 	updated.emit()
+
+func remove_item_at_index(index: int):
+	slots[index] = InventorySlot.new()
+
+func insert_slot(index: int, inventory_slot: InventorySlot):
+	var old_index : int = slots.find(inventory_slot)
+	remove_item_at_index(old_index)
+	
+	slots[index] = inventory_slot
--- scenes/ui/inventory/inventory_gui.gd
+++ scenes/ui/inventory/inventory_gui.gd
@@ -6,15 +6,38 @@ signal closed
 var is_open : bool=false
 
 @onready var inventory : Inventory = preload("res://scenes/objects/inventory/player_inventory.tres")
+@onready var item_stack_gui_class = preload("res://scenes/ui/inventory/item_stack_gui.tscn")
 @onready var slots : Array = $NinePatchRect/GridContainer.get_children()
+ 
+var item_in_hand : ItemStackGui
 
 func  _ready() -> void:
+	connect_slots()
 	inventory.updated.connect(update)
 	update()
 
+func connect_slots():
+	for i in range(slots.size()):
+		var slot = slots[i]
+		slot.index = i
+		
+		var callable = Callable(on_slot_clicked)
+		callable = callable.bind(slot)
+		slot.pressed.connect(callable)
+
 func update():
 	for i in range(min(inventory.slots.size(), slots.size())):
-		slots[i].update(inventory.slots[i])
+		var inventory_slot :InventorySlot = inventory.slots[i]
+		
+		if !inventory_slot.item : continue
+		
+		var item_stack_gui : ItemStackGui = slots[i].item_stack_gui
+		if !item_stack_gui:
+			item_stack_gui = item_stack_gui_class.instantiate()
+			slots[i].insert(item_stack_gui)
+			
+		item_stack_gui.inventory_slot = inventory_slot
+		item_stack_gui.update()
 
 func open():
 	visible = true
@@ -25,3 +48,29 @@ func close():
 	visible = false
 	is_open = false
 	closed.emit()
+
+func on_slot_clicked(slot):
+	if slot.is_empty() && item_in_hand:
+		insert_item_in_slot(slot)
+		return
+	if !item_in_hand:
+		take_item_from_slot(slot)
+
+func take_item_from_slot(slot):
+	item_in_hand = slot.take_item()
+	add_child(item_in_hand)
+	update_item_in_hand()
+
+func insert_item_in_slot(slot):
+	var item = item_in_hand
+	
+	remove_child(item_in_hand)
+	item_in_hand = null
+	slot.insert(item)
+
+func update_item_in_hand():
+	if !item_in_hand :return
+	item_in_hand.global_position = get_global_mouse_position() - item_in_hand.size / 2
+
+func _input(event):
+	update_item_in_hand()
