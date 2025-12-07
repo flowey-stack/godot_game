a7deffd6cb09cd2ee919f9901790e925998f6c5a
s111116037-art <lillian0525flowey@gmail.com>
2025-12-07 22:03:36 +0800

    不弄了

--- scenes/level/day_1/progress_2.gd
+++ scenes/level/day_1/progress_2.gd
@@ -6,6 +6,9 @@ func _ready() -> void:
 	if NavigationManager.spawn_door_tag !=null:
 		_on_level_spawn(NavigationManager.spawn_door_tag)
 	bgm.play()
+	heart_container.set_max_heart(player.max_health)
+	heart_container.update_heart(player.current_health)
+	player.health_changed.connect(heart_container.update_heart)
 
 func _on_level_spawn(destination_tag  : String):
 	var door_path = "Doors/Door_" + destination_tag
--- scenes/level/day_1/progress_4.gd
+++ scenes/level/day_1/progress_4.gd
@@ -4,6 +4,10 @@ extends "res://scenes/level/world/world.gd"
 func _ready() -> void:
 	if NavigationManager.spawn_door_tag !=null:
 		_on_level_spawn(NavigationManager.spawn_door_tag)
+		
+	heart_container.set_max_heart(player.max_health)
+	heart_container.update_heart(player.current_health)
+	player.health_changed.connect(heart_container.update_heart)
 
 func _on_level_spawn(destination_tag  : String):
 	var door_path = "Doors/Door_" + destination_tag
