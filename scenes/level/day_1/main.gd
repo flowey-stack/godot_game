c62366d507e525a2cbb964989d9ba36dda6aadbf
flowey-stack <s111116037@stu.ntue.edu.tw>
2025-12-09 22:53:20 +0800

    對話

--- scenes/level/day_1/main.gd
+++ scenes/level/day_1/main.gd
@@ -3,6 +3,7 @@ extends "res://scenes/level/world/world.gd"
 @onready var bgm = $AudioStreamPlayer2D
 
 func _ready() -> void:
+	Dialogic.start("first_main")
 	if NavigationManager.spawn_door_tag !=null:
 		_on_level_spawn(NavigationManager.spawn_door_tag)
 	bgm.play()
