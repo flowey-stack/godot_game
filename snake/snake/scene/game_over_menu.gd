76a78e5980d7430ce6642e3894ca58043fd3b735
111116032 <152148728+111116032@users.noreply.github.com>
2025-12-15 17:45:12 +0800

    .

--- snake/snake/scene/game_over_menu.gd
+++ snake/snake/scene/game_over_menu.gd
@@ -1,11 +1,11 @@
 extends CanvasLayer
 
-signal restart
+signal exit
 
-func _on_restart_button_pressed() -> void:
+func _on_exit_button_pressed() -> void:
 	#restart.emit()
 	get_tree().change_scene_to_file("res://scenes/level/day_1/main.tscn")
+	
 
 func set_score(value):
 	$GameOverPanel/VBoxContainer/ScoreLabel.text = "SCORE:" + str(value)
-	
