57b809548150e1df0d1d253558aac81cb690512d
s111116037-art <lillian0525flowey@gmail.com>
2025-12-15 22:44:07 +0800

    存一下

--- snake/snake/scene/snake_entrance.gd
+++ snake/snake/scene/snake_entrance.gd
@@ -6,6 +6,14 @@ const PLAYER_GROUP = "Player"
 var player_node = null
 
 func _ready():
+	player_node = get_tree().get_first_node_in_group(PLAYER_GROUP)
+	
+	if player_node == null:
+		# ⚠️ 【診斷訊息】如果找不到，立即警告
+		print("【錯誤警告】在場景 ", get_tree().current_scene.name, " 中找不到群組為 '", PLAYER_GROUP, "' 的玩家節點！")
+	else:
+		print("【診斷】玩家節點查找成功，可以儲存位置。")
+	
 	#確保 Area2D 可以接收滑鼠事件
 	input_pickable = true  
 
@@ -15,7 +23,7 @@ func _input_event(_viewport, event, _shape_idx):
 			GameManager.last_player_position = player_node.global_position
 			print("儲存玩家位置:", GameManager.last_player_position)
 		else:
-			print("警告：找不到玩家節點來儲存位置！")
+			print("【失敗】無法儲存位置，因為 player_node 為 null！")
 			
 		print("切換到場景：", target_scene)
 		get_tree().change_scene_to_file("res://snake/snake/scene/main.tscn")
