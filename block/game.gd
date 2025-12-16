4f3fe0103a92562c98e927da4d028ade7dc9e741
111116032 <152148728+111116032@users.noreply.github.com>
2025-12-17 02:52:23 +0800

    遊戲離開menu

--- block/game.gd
+++ block/game.gd
@@ -1,12 +1,24 @@
 extends Node2D
 
-
 func _ready() -> void:
 	EventBus.crate_reached_dest.connect(_on_crate_reached_dest)
+	
+	if %bCompleteMenu:
+		%bCompleteMenu.hide()
 	
 func _on_crate_reached_dest():
 	for crate: Crate in get_tree().get_nodes_in_group("crates"):
 		if not crate.is_reached:
 			return
 	print("Complete!")
-	get_tree().change_scene_to_file("res://scenes/level/day_1/progress_1.tscn")
+	await get_tree().create_timer(0.5).timeout
+	show_win_menu()
+	
+func show_win_menu():
+	print("正在嘗試顯示選單...")
+	if %bCompleteMenu:
+		%bCompleteMenu.show()
+		get_tree().paused = true
+	else:
+		print("error")
+	
